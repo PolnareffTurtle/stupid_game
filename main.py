b9b1b2711a9bba81fbca81c95fc510d457cb84a5
yechanpark <yechanpark07@gmail.com>
2025-03-14 12:08:19 -0700

    boilerplate

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('hi ming')
+import pygame
+from sys import exit
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((960, 720))
+        pygame.display.set_caption('stupid fucking game')
+        self.clock = pygame.time.Clock()
+
+    def game_running(self):
+        while True:
+            self.screen.fill((123,24,25))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+            self.clock.tick(60)
+            pygame.display.update()
+
+    def run(self):
+        self.game_running()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
